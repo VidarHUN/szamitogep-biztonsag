76ec64eb22ad7f899b37dab7537698f45872d1f3
nono <norbertn503@gmail.com>
2022-11-09 10:33:02 +0100

    deleted from_json, renamed to_json

--- parser/main.cpp
+++ parser/main.cpp
@@ -4,10 +4,10 @@
 #include "parser.hpp"
 
 using namespace std;
+using json = nlohmann::json_abi_v3_11_2::json;
 
-//overrideing to_json function
 //nem lehetett tagfüggvény, csak felül kell írni és meghívni
-inline void to_json(json& j, const ParsedInfo& info){
+void serialize(json& j, const ParsedInfo& info){
     j["blk_info"] = {{"blk_type", info.blk_info.type}, {"blk_length", info.blk_info.length}};
     j["caff_header"] = {{"caff_header_size", info.caff_header.header_size}, 
                        {"caff_header_num_anim", info.caff_header.num_anim}};
@@ -27,40 +27,13 @@ inline void to_json(json& j, const ParsedInfo& info){
                         //{"ciff_header_tags", info.animation->header.tags}}
                         }
                     ///TO DO dinamikus tömböket serializálni header tags is ilyen, asszem
-                  //{"img", info.animation->img}    
+                    //{"img", info.animation->img}    
                     };
 }
 
-//overrideing from_json function
-////nem lehetett tagfüggvény, csak felül kell írni és meghívni
-inline void from_json(const json& j, ParsedInfo& info){
-    j.at("blk_info").at("blk_type").get_to(info.blk_info.type);
-    j.at("blk_info").at("blk_length").get_to(info.blk_info.length);
-    j.at("caff_header").at("caff_header_size").get_to(info.caff_header.header_size);
-    j.at("caff_header").at("caff_header_num_anim").get_to(info.caff_header.num_anim);
-    j.at("caff_credits").at("year").get_to(info.credits.YY);
-    j.at("caff_credits").at("month").get_to(info.credits.M);
-    j.at("caff_credits").at("day").get_to(info.credits.D);
-    j.at("caff_credits").at("hour").get_to(info.credits.h);
-    j.at("caff_credits").at("minute").get_to(info.credits.m);
-    j.at("caff_credits").at("creator").get_to(info.credits.creator);
-    /*TO DO
-    a HIBA [json.exception.type_error.304] cannot use at() with array
-    
-    j.at("animations").at("duration").get_to(info.animation->duration);
-    j.at("animations").at("ciff_header_size").get_to(info.animation->header.header_size);
-    j.at("animations").at("ciff_header_content_size").get_to(info.animation->header.content_size);
-    j.at("animations").at("ciff_header_width").get_to(info.animation->header.width);
-    j.at("animations").at("ciff_header_height").get_to(info.animation->header.height);*/
-    //j.at("animations").at("ciff_header_caption").get_to(info.animation->header.caption);
-    ///TO DO dinamikus ttömbökre
-    //ciff_header_tags
-    //image
-
-}
 
 //prints the json object to stdout 
-static void serialize(const json& j)
+static void serialize_write(const json& j)
 {
     string s = j.dump();
     std::cout << "serialization: " << s << std::endl;
@@ -84,8 +57,8 @@ int main(int argc, char **argv)
         try
         {   
             ParsedInfo info = parser.parse_file(&file);
-            to_json(j, info);
-            //serialize(j);
+            serialize(j, info);
+            serialize_write(j);
         }
         catch (ParserException &e)
         {
@@ -93,12 +66,6 @@ int main(int argc, char **argv)
             file.close();
             return 1;
         }
-
-        ParsedInfo pars;
-        from_json(j,pars);
-        cout << "header len:\t" << pars.blk_info.length << endl;
-        cout << "header len:\t" << pars.caff_header.header_size << endl;
-        cout << "num anim:\t" << pars.caff_header.num_anim << endl;
     }
     file.close();
     return 0;
