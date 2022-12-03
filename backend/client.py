d4e8623d0b085b1eaaf1efaf7d67e4a364647892
Richard Varadi <ricsi19981007@gmail.com>
2022-12-03 14:23:27 +0100

    Implement API endpoints without Parser

--- backend/client.py
+++ backend/client.py
@@ -2,8 +2,14 @@ import os
 import requests
 
 def get_secret_message():
-    response = requests.get("https://localhost:8443", verify="domain.crt")
-    print(f"The secret message is {response.text}")
+    response = requests.delete("https://localhost:8443/request/kratos.gif", verify="domain.crt")
+    print(response.content)
+
+def upload():
+    files = {'file': open('kratos.gif', 'rb')}
+    response = requests.post("https://localhost:8443/request", verify="domain.crt", files=files,)
+    print(response.content)
 
 if __name__ == "__main__":
-    get_secret_message()
+    # get_secret_message()
+    upload()
--- backend/server.py
+++ backend/server.py
@@ -1,8 +1,42 @@
-from flask import Flask
+from flask import Flask, send_file, render_template, request, jsonify
+from werkzeug.utils import secure_filename
+import os
 
-SECRET_MESSAGE = "fluffy tail"
 app = Flask(__name__)
 
-@app.route("/")
-def get_secret_message():
-    return SECRET_MESSAGE
+@app.route("/request", methods=['POST'])
+def postRequest():
+    f = request.files['file']
+    f.save(os.path.join("gifs", secure_filename(f.filename)))
+    gifs = os.listdir("gifs")
+    return jsonify({
+        'res': gifs,
+        'status': '200',
+        'msg': 'Success uploading gif!',
+        'no_of_gifs': len(gifs)
+    })
+
+@app.route("/request/list", methods=['GET'])
+def getRequest():
+    gifs = os.listdir("gifs")
+    return jsonify({
+        'res': gifs,
+        'status': '200',
+        'msg': 'Success getting all the gifs in storage!',
+        'no_of_gifs': len(gifs)
+    })
+
+@app.route("/request/<caff>", methods=['GET'])
+def getRequestCaff(caff):
+    return send_file(f'gifs/{caff}')
+
+@app.route('/request/<caff>', methods=['DELETE'])
+def deleteRequestCaff(caff):
+    os.remove(os.path.join("gifs", secure_filename(caff)))
+    gifs = os.listdir("gifs")
+    return jsonify({
+        'res': gifs,
+        'status': '200',
+        'msg': 'Success deleting gif!',
+        'no_of_gifs': len(gifs)
+    })
