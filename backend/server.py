9e11a33385a9ee3f5c32559d13636b34df7a6e7b
norbertn503 <norbertn503@gmail.com>
2022-12-10 19:52:21 +0100

    server requirements update

--- backend/server.py
+++ backend/server.py
@@ -12,7 +12,7 @@ import jwt
 app = Flask(__name__)
 
 app.config['SECRET_KEY']='004f2af45d3a4e161a7dd2d17fdae47f'
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite://///home/richard/Desktop/BMEM2/szamitogep-biztonsag/backend/users.db'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///D:\\Norbi\\sqlite\\users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app)
